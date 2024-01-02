57ecf8c68d16de17316f0affe3c8defd96d7ec51
iamdumpit <iamdumpit>
2024-01-03 05:37:58 +0800

    Initial Commit

---
+++ band-name-generator-day1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+City = input("What's the name of the city you grew up in?\n")
+Pet = input("What's your pet's name?\n")
+print("Your band name could be {0} {1}".format(City,Pet))
+print("K, Bye, Thanks!")
