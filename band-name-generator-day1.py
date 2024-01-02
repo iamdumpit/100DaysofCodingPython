110a5c6bf8cc0075528f824fc9230d8bc937ccb2
iamdumpit <iamdumpit>
2024-01-03 07:34:40 +0800

    Added another option to print with input in one line

--- band-name-generator-day1.py
+++ band-name-generator-day1.py
@@ -1,4 +1,8 @@
 print("Welcome to the Band Name Generator.")
-City = input("What's the name of the city you grew up in?\n")
-Pet = input("What's your pet's name?\n")
-print("Your band name could be {0} {1}".format(City,Pet))
+# Option 1 - put value into variables
+# City = input("What's the name of the city you grew up in?\n")
+# Pet = input("What's your pet's name?\n")
+# print("Your band name could be {0} {1}".format(City,Pet))
+
+# Other option 2 - using one line of code
+print("Your band name could be {0} {1}".format(input("What's the name of the city you grew up in?\n"), input("What's your pet's name?\n")))
