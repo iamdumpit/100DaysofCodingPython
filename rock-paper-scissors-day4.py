89ec0adbe5a90e6a5a5a89bedadbae53d337330a
iamdumpit <iamdumpit>
2024-01-06 17:09:34 +0800

    Initial Commit

---
+++ rock-paper-scissors-day4.py
@@ -0,0 +1,211 @@
+import random
+from os import system, name
+from time import sleep
+from colorama import Fore, Back, Style
+
+player_score = 0
+cpu_score = 0
+
+
+# define our clear function
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+def show_score(a, b):
+    print("===== Scoreboard =====")
+    print("======================")
+    print(f"Player: {a}\nCPU: {b}")
+    print("======================")
+
+def bet_style(player,cpu):
+    clear()
+    sleep(0.5)
+    print("ROCK\n",rock)
+    sleep(0.75)
+    print("PAPER\n",paper)
+    sleep(0.75)
+    print("SCISSORS\n",scissors)
+    sleep(0.75)
+    print('SHOOT!!')
+    sleep(1)
+    clear()
+
+    if player == 0:
+        print(f"{Fore.LIGHTYELLOW_EX}I chose ROCK!{Style.BRIGHT}")
+        print(f"{Fore.LIGHTYELLOW_EX}{rock}{Style.BRIGHT}")
+    elif player == 1:
+        print(f"{Fore.LIGHTYELLOW_EX}I chose PAPER!{Style.BRIGHT}")
+        print(f"{Fore.LIGHTYELLOW_EX}{paper}{Style.BRIGHT}")
+    else:
+        print(f"{Fore.LIGHTYELLOW_EX}I chose SCISSORS!{Style.BRIGHT}")
+        print(f"{Fore.LIGHTYELLOW_EX}{scissors}{Style.BRIGHT}")
+
+    sleep(0.5)
+    print(f"{Fore.LIGHTWHITE_EX}{versus}{Style.RESET_ALL}\n")
+    sleep(0.5)
+    if cpu == 0:
+        print(f"{Fore.LIGHTRED_EX}CPU chose ROCK!{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTRED_EX}{rock}{Style.RESET_ALL}")
+    elif cpu == 1:
+        print(f"{Fore.LIGHTRED_EX}CPU chose PAPER!{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTRED_EX}{paper}{Style.RESET_ALL}")
+    else:
+        print(f"{Fore.LIGHTRED_EX}CPU chose SCISSORS!{Style.RESET_ALL}")
+        print(f"{Fore.LIGHTRED_EX}{scissors}{Style.RESET_ALL}")
+
+def bet_result(player_bet,cpu_bet):
+    global player_score, cpu_score
+    # Display input, Rock = 0, Paper = 1, Scissors = 2
+    # if Player is Rock and CPU is Paper, Player loses
+    if player_bet == 0 and cpu_bet == 2:
+        print(f'{Fore.LIGHTGREEN_EX}You win!\n{Style.DIM}')
+        player_score += 1
+    # if Player is Rock and CPU is Scissors, Player wins
+    elif cpu_bet == 0 and player_bet == 2:
+        print(f'{Fore.LIGHTGREEN_EX}You lose!\n{Style.DIM}')
+        cpu_score += 1
+    # if both picked Rock, Draw
+    elif player_bet > cpu_bet:
+        print(f'{Fore.LIGHTGREEN_EX}You win!\n{Style.DIM}')
+        player_score += 1
+    elif cpu_bet > player_bet:
+        print(f'{Fore.LIGHTGREEN_EX}You lose!\n{Style.DIM}')
+        cpu_score += 1
+    elif player_bet == cpu_bet:
+        print(f'{Fore.LIGHTGREEN_EX}DRAW!\n{Style.DIM}')
+    show_score(player_score,cpu_score)
+def main():
+
+    # Initialize try again
+    try_again = "Y"
+
+    print(f'{Fore.LIGHTGREEN_EX}{hello_text}{Style.DIM}')
+    while try_again == "Y":
+        try:
+            player_bet = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \n"))
+            cpu_bet = random.randint(0, 2)
+
+            if player_bet >= 3 or player_bet < 0:
+                print("Wrong option! Type only 0 for Rock, 1 for Paper or 2 for Scissors.")
+            else:
+                bet_style(player_bet,cpu_bet)
+                bet_result(player_bet,cpu_bet)
+        except:
+            pass
+        try_again = input("Try again?\n").upper()
+        clear()
+    print(f'{Fore.LIGHTGREEN_EX}{thank_you}{Style.DIM}')
+    return 0
+
+
+# Define ASCII arts below
+
+rock = '''
+
+⠀⠀⠀⠀⠀⣠⡴⠖⠒⠲⠶⢤⣄⡀⠀⠀⠀
+⠀⠀⠀⢀⡾⠁⠀⣀⠔⠁⠀⠀⠈⠙⠷⣤⠦⣤⡀
+⣠⠞⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⠈⢿⡀
+⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⠛⠛⠃⠸⡇⠈⣇
+⣹⡷⠤⠤⠤⠄⠀⠀⠀⠀⢠⣤⡤⠶⠖⠛⠀⣿⠀⣿
+⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡤⠖⠋⢀⣿⣠⠏
+⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠁
+⠀⠉⢿⡋⠉⠉⠁⠀⠀⠀⠀⠀⢀⣠⠾⠋
+⠀⠀⠈⠛⠶⠦⠤⠤⠤⠶⠶⠛⠋⠁
+'''
+
+paper = '''
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠖⠒⢶⣄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠋⠀⠀⠀⢀⡿⠀⠀⠀⠀⠀⠀⠀
+⢠⡶⠒⠳⠶⣄⠀⠀⠀⠀⠀⣴⠟⠁⠀⠀⠀⣰⠏⠀⢀⣤⣤⣄⡀⠀⠀
+⠸⡇⠀⠀⠀⠘⣇⠀⠀⣠⡾⠁⠀⠀⠀⢀⣾⣣⡴⠚⠉⠀⠀⠈⠹⡆⠀
+⠀⢻⡄⠀⠀⠀⢻⣠⡾⠋⠀⠀⠀⠀⣠⡾⠋⠁⠀⠀⠀⠀⢀⣠⡾⠃⠀
+⠀⣿⠀⠀⠀⠘⠉⠀⠀⠀⠀⠀⡰⠋⠀⠀⠀⠀⠀⣠⠶⠋⠁⠀⠀⠀
+⠠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠁⠀⠀⠀⢀⣴⡿⠥⠶⠖⠛⠛⢶⡄
+⢀⣰⡇⠀⠀⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⢀⣠⠼⠃
+⣿⠉⣇⠀⡴⠟⠁⣠⡾⠃⠀⠀⠀⠀⠀⠈⠀⠀⠀⣀⣤⠶⠛⠉⠀⠀⠀
+⢻⡄⠹⣦⠀⠶⠛⢁⣠⡴⠀⠀⠀⠀⠀⠀⣠⡶⠛⠉⠀⠀⠀⠀⠀⠀⠀
+⠻⣄⠈⢷⣄⠈⠉⠁⠀⠀⠀⢀⣠⡴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠉⠳⢤⣭⡿⠒⠶⠶⠒⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+'''
+
+scissors = '''
+⠀⠀⠀⠀⢀⣠⣤⣀⣠⣤⠶⠶⠒⠶⠶⣤⣀
+⠀⠀⢀⡴⠋⣠⠞⠋⠁⠀⠀⠀⠀⠙⣄⠀⠙⢷⡀
+⠀⢀⡾⠁⣴⠋⠰⣤⣄⡀⠀⠀⠀⠀⠈⠳⢤⣼⣇⣀⣀
+⠀⢸⠃⢰⠇⠰⢦⣄⡈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠛⠛⠓⠲⢦⣄
+⠀⠸⣧⣿⠀⠻⣤⡈⠛⠳⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⢻⡆
+⠀⠀⠈⠹⣆⠀⠈⠛⠂⠀⠀⠀⠀⠀⠀⠈⠐⠒⠒⠶⣶⣶⠶⠤⠤⣤⣠⡼⠃
+⠀⠀⠀⠀⠹⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⢦⣀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠻⣦⣀⠀⠀⠀⠀⠐⠲⠤⣤⣀⡀⠀⠀⠀⠀⠀⠉⢳⡄
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠶⠤⠤⠤⠶⠞⠋⠉⠙⠳⢦⣄⡀⠀⠀⠀⡷
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠳⠦⠾⠃⠀⠀
+'''
+
+hello_text = '''
+
+$$\   $$\ $$$$$$$$\ $$\       $$\       $$$$$$\    
+$$ |  $$ |$$  _____|$$ |      $$ |     $$  __$$\   
+$$ |  $$ |$$ |      $$ |      $$ |     $$ /  $$ |  
+$$$$$$$$ |$$$$$\    $$ |      $$ |     $$ |  $$ |  
+$$  __$$ |$$  __|   $$ |      $$ |     $$ |  $$ |  
+$$ |  $$ |$$ |      $$ |      $$ |     $$ |  $$ |  
+$$ |  $$ |$$$$$$$$\ $$$$$$$$\ $$$$$$$$\ $$$$$$  |  
+\__|  \__|\________|\________|\________|\______/  
+
+===================LETS PLAY!===================
+
+⠀⠀⠀⠀⠀⣠⡴⠖⠒⠲⠶⢤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠖⠒⢶⣄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⡾⠁⠀⣀⠔⠁⠀⠀⠈⠙⠷⣤⠦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠋⠀⠀⠀⢀⡿⠀⠀⠀⠀⠀⠀⠀
+⣠⠞⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠘⢧⠈⢿⡀⢠⡶⠒⠳⠶⣄⠀⠀⠀⠀⠀⣴⠟⠁⠀⠀⠀⣰⠏⠀⢀⣤⣤⣄⡀⠀⠀
+⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⠛⠛⠃⠸⡇⠈⣇⠸⡇⠀⠀⠀⠘⣇⠀⠀⣠⡾⠁⠀⠀⠀⢀⣾⣣⡴⠚⠉⠀⠀⠈⠹⡆⠀
+⣹⡷⠤⠤⠤⠄⠀⠀⠀⠀⢠⣤⡤⠶⠖⠛⠀⣿⠀⣿⠀⢻⡄⠀⠀⠀⢻⣠⡾⠋⠀⠀⠀⠀⣠⡾⠋⠁⠀⠀⠀⠀⢀⣠⡾⠃⠀
+⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡤⠖⠋⢀⣿⣠⠏⠀⠀⣿⠀⠀⠀⠘⠉⠀⠀⠀⠀⠀⡰⠋⠀⠀⠀⠀⠀⣠⠶⠋⠁⠀⠀⠀
+⢿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠁⠀⠀⠠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠁⠀⠀⠀⢀⣴⡿⠥⠶⠖⠛⠛⢶⡄
+⠀⠉⢿⡋⠉⠉⠁⠀⠀⠀⠀⠀⢀⣠⠾⠋⠀⠀⠀⠀⢀⣰⡇⠀⠀⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⢀⣠⠼⠃
+⠀⠀⠈⠛⠶⠦⠤⠤⠤⠶⠶⠛⠋⠁⠀⠀⠀⠀⠀⠀⣿⠉⣇⠀⡴⠟⠁⣠⡾⠃⠀⠀⠀⠀⠀⠈⠀⠀⠀⣀⣤⠶⠛⠉⠀⠀⠀
+⠀⠀⠀⠀⢀⣠⣤⣀⣠⣤⠶⠶⠒⠶⠶⣤⣀⠀⠀⠀⢻⡄⠹⣦⠀⠶⠛⢁⣠⡴⠀⠀⠀⠀⠀⠀⣠⡶⠛⠉⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⡴⠋⣠⠞⠋⠁⠀⠀⠀⠀⠙⣄⠀⠙⢷⡀⠀⠀⠻⣄⠈⢷⣄⠈⠉⠁⠀⠀⠀⢀⣠⡴⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⡾⠁⣴⠋⠰⣤⣄⡀⠀⠀⠀⠀⠈⠳⢤⣼⣇⣀⣀⠀⠉⠳⢤⣭⡿⠒⠶⠶⠒⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢸⠃⢰⠇⠰⢦⣄⡈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠛⠛⠓⠲⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠸⣧⣿⠀⠻⣤⡈⠛⠳⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠹⣆⠀⠈⠛⠂⠀⠀⠀⠀⠀⠀⠈⠐⠒⠒⠶⣶⣶⠶⠤⠤⣤⣠⡼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠹⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⢦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠻⣦⣀⠀⠀⠀⠀⠐⠲⠤⣤⣀⡀⠀⠀⠀⠀⠀⠉⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠶⠤⠤⠤⠶⠞⠋⠉⠙⠳⢦⣄⡀⠀⠀⠀⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠳⠦⠾⠃⠀⠀⠀⠀
+⠀⠀
+================================================⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+'''
+
+versus = '''
+__     ______  
+\ \   / / ___| 
+ \ \ / /\___ \ 
+  \ V /  ___) |
+   \_/  |____/ 
+'''
+
+thank_you = '''
+
+ _____ _                 _                           __            
+|_   _| |__   __ _ _ __ | | __  _   _  ___  _   _   / _| ___  _ __ 
+  | | | '_ \ / _` | '_ \| |/ / | | | |/ _ \| | | | | |_ / _ \| '__|
+  | | | | | | (_| | | | |   <  | |_| | (_) | |_| | |  _| (_) | |   
+  |_| |_| |_|\__,_|_| |_|_|\_\  \__, |\___/ \__,_| |_|  \___/|_|   
+       _             _          |___/                              
+ _ __ | | __ _ _   _(_)_ __   __ _| |                              
+| '_ \| |/ _` | | | | | '_ \ / _` | |                              
+| |_) | | (_| | |_| | | | | | (_| |_|                              
+| .__/|_|\__,_|\__, |_|_| |_|\__, (_)                              
+|_|            |___/         |___/                                 
+
+'''
+
+if __name__ == "__main__":
+    main()
